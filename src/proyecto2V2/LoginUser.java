49a7db66afda92cb1b4e0fcbc7e072c7c32325b3
FabianDiazB <fabiandiazbarboza@gmail.com>
2022-11-10 01:37:37 -0600

    2w

--- src/proyecto2V2/LoginUser.java
+++ src/proyecto2V2/LoginUser.java
@@ -29,7 +29,6 @@ public class LoginUser extends javax.swing.JFrame {
     }
      
      public void iniciarJuego() throws IOException, ClassNotFoundException{
-         System.out.println("a");
           for( Usuario u : usuarios.getUsers()){
               if(txfUsuario.getText().equals(u.getNombre())){
                   GUIUsuario pantalla = new GUIUsuario(u,usuarios);
